3c61eb92adcd156c0d19ca32aa83ba0e11d72d34
Jones, Ben <bmjones@mitre.org>
2017-08-08 14:07:31 -0400

    dev v3 typos fixeds

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
 	
     private static String version = "3.2.2";
-    private static String featureList = "* point releases\n*master version";
+    private static String featureList = "* point releases\n* master version";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World.\n This is version:"+version+"\nNow With Features:\n"+featureList);
+        System.out.println("Hello, World.\nThis is version:"+version+"\nNow With Features:\n"+featureList);
     }
 
 }
