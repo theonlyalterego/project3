1b7233a22ac0125983ea344226206a42e4b380bf
Jones, Ben <bmjones@mitre.org>
2017-08-08 13:34:44 -0400

    commiting v3

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	
+    private static String version = "3";
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World. This is version:"+version);
+    }
+
+}
