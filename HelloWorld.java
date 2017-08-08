afd3c3c88875122c598f4390dc094f610e57660b
Jones, Ben <bmjones@mitre.org>
2017-08-08 13:37:26 -0400

    Hello World: implemented point relese and release notes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld {
 	
-    private static String version = "3.1.0";
+    private static String version = "3.2.0";
+    private static String featureList = "feature list\npoint releases";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World. This is version:"+version);
+        System.out.println("Hello, World. This is version:"+version+"\n Now With Features:\n"+featureList);
     }
 
 }
